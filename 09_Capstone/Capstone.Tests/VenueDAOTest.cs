5591719aa51aa53550f2b18ed7d9047d6bb7e70a
Kathryn Moon <kathryn.moon.7@gmail.com>
2021-02-19 11:09:39 -0500

    M2-09 Beginning Tests

--- 09_Capstone/Capstone.Tests/VenueDAOTest.cs
+++ 09_Capstone/Capstone.Tests/VenueDAOTest.cs
@@ -1,4 +1,6 @@
+using Capstone.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
 
 namespace Capstone.Tests
 {
@@ -14,6 +16,37 @@ namespace Capstone.Tests
 
             //Assert
             Assert.IsNotNull(venueDAO);
+
+        }
+
+        [TestMethod]
+        [DataRow("SELECT TOP 1 * FROM venue ORDER BY name ASC;", 6)]
+        [DataRow("SELECT TOP 1 * FROM venue ORDER BY name DESC;", 13)]
+        public void DoesReaderConvertToVenue(string sqlCommand, int expectedId)
+        {
+            //Arrange
+            Venue tempVenue = new Venue();
+
+            //Act
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sqlCommand, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        tempVenue = venueDAO.ConvertReaderToVenue(reader);
+                    }
+                }
+            }
+            catch (SqlException ex) { }
+
+            //Assert
+            Assert.IsNotNull(tempVenue);
+            Assert.AreEqual(expectedId, tempVenue.ID);
 
         }
     }
